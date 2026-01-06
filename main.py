0f3f8ebc44f2d67bd4894fd2ab95cc2c185ecb76
Seiya0106 <g2553458@cc.kyoto-su.ac.jp>
2026-01-06 11:58:37 +0900

    Add; 画像のクリック判定

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
 import pygame as pg
 import sys
+import random
 
 pg.init()
-screen = pg.display.set_mode((800, 600))
+WIDTH = 800
+HEIGHT = 600
+screen = pg.display.set_mode((WIDTH, HEIGHT))
+font = pg.font.SysFont(None, 36)
+img1 = pg.image.load("assets/button.png")
+button = pg.transform.scale(img1, (150, 120))
+pos = (random.randint(0, WIDTH), random.randint(0, HEIGHT))
+button_rect = pg.Rect(pos[0], pos[1], 150, 120)
 
 while True:
     screen.fill(pg.Color("WHITE"))
+    mouse = pg.mouse.get_pressed()
+    mouse_pos = pg.mouse.get_pos()
 
-    pg.draw.rect(screen, pg.Color("RED"), (100, 100, 100, 150))
+    screen.blit(button, pos)
+    if mouse[0]:
+        if button_rect.collidepoint(mouse_pos):
+            text = font.render("Clear", True, pg.Color("RED"))
+            screen.blit(text, (10, 10))
 
     pg.display.update()
 
