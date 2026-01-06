b9ae2ee2dfe3003b2669e3589abbf0ce3257c47b
Seiya0106 <g2553458@cc.kyoto-su.ac.jp>
2026-01-06 09:47:31 +0900

    ファイルの準備

---
+++ main.py
@@ -0,0 +1,18 @@
+import pygame as pg
+import sys
+
+pg.init()
+screen = pg.display.set_mode((800, 600))
+
+while True:
+    screen.fill(pg.Color("WHITE"))
+
+    pg.draw.rect(screen, pg.Color("RED"), (100, 100, 100, 150))
+
+    pg.display.update()
+
+    # 閉じるボタンで終了
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            pg.quit()
+            sys.exit()
