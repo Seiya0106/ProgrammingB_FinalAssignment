a9780ad23c4ad908c5e1b14debfe9185308e2274
Seiya0106 <g2553458@cc.kyoto-su.ac.jp>
2026-01-07 11:50:32 +0900

    Update; README

--- player.py
+++ player.py
@@ -11,7 +11,7 @@ game_state = game_state_manager.state
 timer = game_state_manager.timer
 
 def clicked(rect, mouse_pos, mouse_buttons):
-    if mouse_buttons[0]: 
+    if mouse_buttons[0]:
         if rect.collidepoint(mouse_pos):
             return True
     return False
